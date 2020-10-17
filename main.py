14c6e789e9200e6088fcd64dd23d07a6c0af6928
Matviichuk <matviichuk.olexandr@gmail.com>
2020-10-17 14:06:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,5 @@ from bot_shared import DISPATCHER
 from aiogram import executor
 import bot_command_handlers
 
-# DEFAULT_STORAGE.register_privileged_user_with_id(374760184)
-# DEFAULT_STORAGE.register_privileged_user_with_id(152433470)
-
 if __name__ == '__main__':
     executor.start_polling(DISPATCHER, skip_updates=True)
